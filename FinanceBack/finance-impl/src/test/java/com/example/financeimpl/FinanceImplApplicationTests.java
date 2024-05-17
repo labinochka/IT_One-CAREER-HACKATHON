f3d74857a6327a1aebdf03ef7a27059238f957e2
labinochka <labensckayam@yandex.ru>
2024-05-17 21:25:18 +0300

    feat: add project

---
+++ FinanceBack/finance-impl/src/test/java/com/example/financeimpl/FinanceImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.financeimpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceImplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
