84eaf427c6fdba8876cf38bfe7ea3e6f58ebcfc7
Dmitriy Larin <larinbase@mail.ru>
2024-05-17 22:30:36 +0300

    fix: project settings

--- FinanceBack/finance-impl/src/main/java/ru/itis/financeimpl/FinanceImplApplication.java
+++ FinanceBack/finance-impl/src/main/java/ru/itis/financeimpl/FinanceImplApplication.java
@@ -1,13 +1,13 @@
-package com.example.financeapi;
+package ru.itis.financeimpl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinanceApiApplication {
+public class FinanceImplApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FinanceApiApplication.class, args);
+        SpringApplication.run(FinanceImplApplication.class, args);
     }
 
 }
