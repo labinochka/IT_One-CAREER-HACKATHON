9e30a2489532a3dc9ce8a3bd39497ad198d6c68b
labinochka <labensckayam@yandex.ru>
2024-05-18 09:18:51 +0300

    added controller and service

--- FinanceBack/finance-impl/src/main/java/ru/itis/financeimpl/repository/TransactionRepository.java
+++ FinanceBack/finance-impl/src/main/java/ru/itis/financeimpl/repository/TransactionRepository.java
@@ -1,9 +1,8 @@
 package ru.itis.financeimpl.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import ru.itis.financeimpl.model.Transaction;
 
 import java.util.UUID;
 
-public interface TransactionRepository extends JpaRepository<UUID, Transaction> {
+public interface TransactionRepository  {
 }
