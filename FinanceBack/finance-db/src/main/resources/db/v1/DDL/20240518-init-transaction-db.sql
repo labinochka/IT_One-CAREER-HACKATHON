d31e534f284faa1c48d5527790b71d199a185869
labinochka <labensckayam@yandex.ru>
2024-05-18 12:19:48 +0300

    add account migration

--- FinanceBack/finance-db/src/main/resources/db/v1/DDL/20240518-init-transaction-db.sql
+++ FinanceBack/finance-db/src/main/resources/db/v1/DDL/20240518-init-transaction-db.sql
@@ -1,8 +1,8 @@
 create table transaction (
-    id uuid primary key,
+    id uuid primary key default uuid_generate_v4(),
     create_date timestamp with time zone not null,
     update_date timestamp with time zone not null,
---     account_id uuid not null references account(id),
+    account_id uuid not null references account(id),
     date timestamp not null,
     amount integer not null,
     category varchar(255) not null,
