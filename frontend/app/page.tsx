1444d0fb3891f3147963c20052e405e21a416f3a
Daniel <perzhul.works@gmail.com>
2024-05-19 11:15:57 +0300

    feat: goals page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -59,7 +59,7 @@ export default function Dashboard() {
   );
 }
 
-const Header = () => {
+export const Header = () => {
   return (
     <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
       <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
