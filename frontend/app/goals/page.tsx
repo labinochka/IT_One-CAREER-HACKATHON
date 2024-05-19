23d7f0fcd189ce593f9ced534578a35acd9daef2
Daniel <perzhul.works@gmail.com>
2024-05-19 11:29:09 +0300

    feat: goals

--- frontend/app/goals/page.tsx
+++ frontend/app/goals/page.tsx
@@ -1,11 +1,30 @@
 "use client";
 
+import { Badge } from "@/components/ui/badge";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Header } from "../page";
 import { Button } from "@/components/ui/button";
 import { api } from "@/shared/api";
 import { useState } from "react";
+import { Progress } from "@/components/ui/progress";
 
 export default function Component() {
   const [goal, setGoal] = useState<number>(0);
@@ -30,55 +49,246 @@ export default function Component() {
   return (
     <div>
       <Header />
-      <main className="max-w-screen-md mx-auto w-full p-4">
-        <div className="relative hidden flex-col items-start gap-8 md:flex">
-          <form
-            onSubmit={handleSubmit}
-            className="grid w-full items-start gap-6"
-          >
-            <h2 className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0">
-              Создать цель и лимит бюджета
-            </h2>
-            <fieldset className="grid gap-6 rounded-lg border p-4">
-              <div className="grid gap-3">
-                <Label htmlFor="goal">Цель заработка</Label>
-                <Input
-                  onChange={(e) => setGoal(Number(e.target.value))}
-                  id="goal"
-                  type="number"
-                />
-              </div>
-              <div className="grid gap-3">
-                <Label htmlFor="expense">Цель расходов</Label>
-                <Input
-                  onChange={(e) => setExpenseGoal(Number(e.target.value))}
-                  id="expense"
-                  type="number"
-                />
-              </div>
-              <div className="grid gap-3">
-                <Label htmlFor="budget-limit">Лимит бюджета</Label>
-                <Input
-                  onChange={(e) => setExpenseGoal(Number(e.target.value))}
-                  id="bugdet-limit"
-                  type="number"
-                />
-              </div>
-              <div className="grid grid-cols-2 gap-4">
+      <main className="mx-auto w-full p-4 ">
+        <h2 className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0 mb-4">
+          Создать цель и лимит бюджета
+        </h2>
+
+        <div className="grid gap-6 grid-flow-col w-full">
+          <div className="relative hidden flex-col items-start gap-8 md:flex">
+            <div className="flex w-full gap-4">
+              <Card className="w-1/2">
+                <CardHeader className="pb-2">
+                  <CardDescription>This Week</CardDescription>
+                  <CardTitle className="text-4xl">$1,329</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <div className="text-xs text-muted-foreground">
+                    +25% from last week
+                  </div>
+                </CardContent>
+                <CardFooter>
+                  <Progress value={25} aria-label="25% increase" />
+                </CardFooter>
+              </Card>
+              <Card className="w-1/2">
+                <CardHeader className="pb-2">
+                  <CardDescription>This Week</CardDescription>
+                  <CardTitle className="text-4xl">$1,329</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <div className="text-xs text-muted-foreground">
+                    +25% from last week
+                  </div>
+                </CardContent>
+                <CardFooter>
+                  <Progress value={25} aria-label="25% increase" />
+                </CardFooter>
+              </Card>
+            </div>
+
+            <form
+              onSubmit={handleSubmit}
+              className="grid w-full items-start gap-6"
+            >
+              <fieldset className="grid gap-6 rounded-lg border p-4">
                 <div className="grid gap-3">
-                  <Label htmlFor="start-date">Дата начала</Label>
-                  <Input id="start-date" type="date" />
+                  <Label htmlFor="goal">Цель заработка</Label>
+                  <Input
+                    onChange={(e) => setGoal(Number(e.target.value))}
+                    id="goal"
+                    type="number"
+                  />
                 </div>
                 <div className="grid gap-3">
-                  <Label htmlFor="end-date">Дата окончания</Label>
-                  <Input id="end-date" type="date" />
+                  <Label htmlFor="expense">Цель расходов</Label>
+                  <Input
+                    onChange={(e) => setExpenseGoal(Number(e.target.value))}
+                    id="expense"
+                    type="number"
+                  />
                 </div>
-              </div>
-              <Button type="submit">Создать</Button>
-            </fieldset>
-          </form>
+                <div className="grid gap-3">
+                  <Label htmlFor="budget-limit">Лимит бюджета</Label>
+                  <Input
+                    onChange={(e) => setExpenseGoal(Number(e.target.value))}
+                    id="bugdet-limit"
+                    type="number"
+                  />
+                </div>
+                <div className="grid grid-cols-2 gap-4">
+                  <div className="grid gap-3">
+                    <Label htmlFor="start-date">Дата начала</Label>
+                    <Input id="start-date" type="date" />
+                  </div>
+                  <div className="grid gap-3">
+                    <Label htmlFor="end-date">Дата окончания</Label>
+                    <Input id="end-date" type="date" />
+                  </div>
+                </div>
+                <Button type="submit">Создать</Button>
+              </fieldset>
+            </form>
+          </div>
+          <List />
         </div>
       </main>
     </div>
+  );
+}
+
+export function List() {
+  return (
+    <Card>
+      <CardHeader className="px-7">
+        <CardTitle>Orders</CardTitle>
+        <CardDescription>Recent orders from your store.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Customer</TableHead>
+              <TableHead className="hidden sm:table-cell">Type</TableHead>
+              <TableHead className="hidden sm:table-cell">Status</TableHead>
+              <TableHead className="hidden md:table-cell">Date</TableHead>
+              <TableHead className="text-right">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow className="bg-accent">
+              <TableCell>
+                <div className="font-medium">Liam Johnson</div>
+                <div className="hidden text-sm text-muted-foreground md:inline">
+                  liam@example.com
+                </div>
+              </TableCell>
+              <TableCell className="hidden sm:table-cell">Sale</TableCell>
+              <TableCell className="hidden sm:table-cell">
+                <Badge className="text-xs" variant="secondary">
+                  Fulfilled
+                </Badge>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">2023-06-23</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <div className="font-medium">Olivia Smith</div>
+                <div className="hidden text-sm text-muted-foreground md:inline">
+                  olivia@example.com
+                </div>
+              </TableCell>
+              <TableCell className="hidden sm:table-cell">Refund</TableCell>
+              <TableCell className="hidden sm:table-cell">
+                <Badge className="text-xs" variant="outline">
+                  Declined
+                </Badge>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">2023-06-24</TableCell>
+              <TableCell className="text-right">$150.00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <div className="font-medium">Noah Williams</div>
+                <div className="hidden text-sm text-muted-foreground md:inline">
+                  noah@example.com
+                </div>
+              </TableCell>
+              <TableCell className="hidden sm:table-cell">
+                Subscription
+              </TableCell>
+              <TableCell className="hidden sm:table-cell">
+                <Badge className="text-xs" variant="secondary">
+                  Fulfilled
+                </Badge>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">2023-06-25</TableCell>
+              <TableCell className="text-right">$350.00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <div className="font-medium">Emma Brown</div>
+                <div className="hidden text-sm text-muted-foreground md:inline">
+                  emma@example.com
+                </div>
+              </TableCell>
+              <TableCell className="hidden sm:table-cell">Sale</TableCell>
+              <TableCell className="hidden sm:table-cell">
+                <Badge className="text-xs" variant="secondary">
+                  Fulfilled
+                </Badge>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">2023-06-26</TableCell>
+              <TableCell className="text-right">$450.00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <div className="font-medium">Liam Johnson</div>
+                <div className="hidden text-sm text-muted-foreground md:inline">
+                  liam@example.com
+                </div>
+              </TableCell>
+              <TableCell className="hidden sm:table-cell">Sale</TableCell>
+              <TableCell className="hidden sm:table-cell">
+                <Badge className="text-xs" variant="secondary">
+                  Fulfilled
+                </Badge>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">2023-06-23</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <div className="font-medium">Liam Johnson</div>
+                <div className="hidden text-sm text-muted-foreground md:inline">
+                  liam@example.com
+                </div>
+              </TableCell>
+              <TableCell className="hidden sm:table-cell">Sale</TableCell>
+              <TableCell className="hidden sm:table-cell">
+                <Badge className="text-xs" variant="secondary">
+                  Fulfilled
+                </Badge>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">2023-06-23</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <div className="font-medium">Olivia Smith</div>
+                <div className="hidden text-sm text-muted-foreground md:inline">
+                  olivia@example.com
+                </div>
+              </TableCell>
+              <TableCell className="hidden sm:table-cell">Refund</TableCell>
+              <TableCell className="hidden sm:table-cell">
+                <Badge className="text-xs" variant="outline">
+                  Declined
+                </Badge>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">2023-06-24</TableCell>
+              <TableCell className="text-right">$150.00</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>
+                <div className="font-medium">Emma Brown</div>
+                <div className="hidden text-sm text-muted-foreground md:inline">
+                  emma@example.com
+                </div>
+              </TableCell>
+              <TableCell className="hidden sm:table-cell">Sale</TableCell>
+              <TableCell className="hidden sm:table-cell">
+                <Badge className="text-xs" variant="secondary">
+                  Fulfilled
+                </Badge>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">2023-06-26</TableCell>
+              <TableCell className="text-right">$450.00</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
   );
 }
